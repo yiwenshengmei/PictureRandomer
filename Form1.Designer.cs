3e2f3f565fc2e05243b5dac75378c13b86f1af07
jzhao <yiwenshengmei@163.com>
2012-03-03 22:06:19 +0800

    下一张图片的操作调整为鼠标左键或键盘左键，上一张图片的操作调整为键盘右键。 增加另存功能。

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,5 +1,5 @@
 ﻿namespace PictureRandomer {
-	partial class Form1 {
+	partial class MainForm {
 		/// <summary>
 		/// 必需的设计器变量。
 		/// </summary>
@@ -23,14 +23,20 @@
 		/// 使用代码编辑器修改此方法的内容。
 		/// </summary>
 		private void InitializeComponent() {
-			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.components = new System.ComponentModel.Container();
 			this.pictureBox1 = new System.Windows.Forms.PictureBox();
-			this.label1 = new System.Windows.Forms.Label();
+			this.pictureContextMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.menuSaveAs = new System.Windows.Forms.ToolStripMenuItem();
+			this.lblMessage = new System.Windows.Forms.Label();
+			this.dlgPictureOutputSelector = new System.Windows.Forms.FolderBrowserDialog();
+			this.loadPictureWorker = new System.ComponentModel.BackgroundWorker();
 			((System.ComponentModel.ISupportInitialize) (this.pictureBox1)).BeginInit();
+			this.pictureContextMenu.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// pictureBox1
 			// 
+			this.pictureBox1.ContextMenuStrip = this.pictureContextMenu;
 			this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.pictureBox1.Location = new System.Drawing.Point(0, 0);
 			this.pictureBox1.Name = "pictureBox1";
@@ -40,29 +46,44 @@
 			this.pictureBox1.TabStop = false;
 			this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
 			// 
-			// label1
+			// pictureContextMenu
 			// 
-			this.label1.AutoSize = true;
-			this.label1.Dock = System.Windows.Forms.DockStyle.Bottom;
-			this.label1.Location = new System.Drawing.Point(0, 435);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(43, 17);
-			this.label1.TabIndex = 1;
-			this.label1.Text = "label1";
+			this.pictureContextMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuSaveAs});
+			this.pictureContextMenu.Name = "pcitureContextMenu";
+			this.pictureContextMenu.Size = new System.Drawing.Size(153, 48);
 			// 
-			// Form1
+			// menuSaveAs
+			// 
+			this.menuSaveAs.Name = "menuSaveAs";
+			this.menuSaveAs.Size = new System.Drawing.Size(152, 22);
+			this.menuSaveAs.Text = "另存为...(&S)";
+			this.menuSaveAs.Click += new System.EventHandler(this.menuSaveAs_Click);
+			// 
+			// lblMessage
+			// 
+			this.lblMessage.AutoSize = true;
+			this.lblMessage.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.lblMessage.Location = new System.Drawing.Point(0, 435);
+			this.lblMessage.Name = "lblMessage";
+			this.lblMessage.Size = new System.Drawing.Size(43, 17);
+			this.lblMessage.TabIndex = 1;
+			this.lblMessage.Text = "label1";
+			// 
+			// MainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(334, 452);
-			this.Controls.Add(this.label1);
+			this.Controls.Add(this.lblMessage);
 			this.Controls.Add(this.pictureBox1);
 			this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (134)));
-			this.Icon = ((System.Drawing.Icon) (resources.GetObject("$this.Icon")));
-			this.Name = "Form1";
-			this.Text = "Form1";
+			this.Name = "MainForm";
+			this.Text = "随机图片";
 			this.Load += new System.EventHandler(this.Form1_Load);
+			this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MainForm_KeyDown);
 			((System.ComponentModel.ISupportInitialize) (this.pictureBox1)).EndInit();
+			this.pictureContextMenu.ResumeLayout(false);
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -71,7 +92,11 @@
 		#endregion
 
 		private System.Windows.Forms.PictureBox pictureBox1;
-		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label lblMessage;
+		private System.Windows.Forms.FolderBrowserDialog dlgPictureOutputSelector;
+		private System.Windows.Forms.ContextMenuStrip pictureContextMenu;
+		private System.Windows.Forms.ToolStripMenuItem menuSaveAs;
+		private System.ComponentModel.BackgroundWorker loadPictureWorker;
 	}
 }
 
--- Form1.cs
+++ Form1.cs
@@ -10,23 +10,53 @@ using System.IO;
 using System.Diagnostics;
 
 namespace PictureRandomer {
-	public partial class Form1: Form {
+	public partial class MainForm: Form {
 		private Random m_randomer = new Random();
 		private String m_imageFolder = @"D:\我的文档\My Pictures\人物\";
 		private List<String> passed_real_path = new List<string>();
 		private List<String> m_images = new List<String>();
-		private int imageIndex = -1;
+		private int m_imageIndex = -1;
+		private string m_outputFolder = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
 
-		public Form1() {
+		public MainForm() {
 			InitializeComponent();
+			dlgPictureOutputSelector.RootFolder = Environment.SpecialFolder.Desktop;
+			m_imageIndex = 0;
 		}
 
+		// ### 事件捕捉
 		private void Form1_Load(object sender, EventArgs e) {
-			RandomPicture2List();
-			imageIndex = 0;
-			SetPictureBox(m_images[imageIndex]);
+			lblMessage.Text = "正在加载图片......";
+			loadPictureWorker.DoWork += (object worker, DoWorkEventArgs worker_e) => {
+				RandomPicture2List();
+			};
+			loadPictureWorker.RunWorkerCompleted += (object worker, RunWorkerCompletedEventArgs worker_e) => {
+				ClearText();
+				SetPictureBox(m_images[m_imageIndex]);
+			};
+			loadPictureWorker.RunWorkerAsync();
+		}
+		private void pictureBox1_MouseClick(object sender, MouseEventArgs e) {
+			if (e.Button == System.Windows.Forms.MouseButtons.Left) {
+				GoNextPicture();
+			} else if (e.Button == System.Windows.Forms.MouseButtons.Right) {
+				// GoPreviousPicture();
+			}
+		}
+		private void MainForm_KeyDown(object sender, KeyEventArgs e) {
+			if (e.KeyCode == Keys.Right) {
+				GoNextPicture();
+			} else if (e.KeyCode == Keys.Left) {
+				GoPreviousPicture();
+			} else if (e.KeyCode == Keys.S && e.Control) {
+				SaveAs();
+			}
+		}
+		private void menuSaveAs_Click(object sender, EventArgs e) {
+			SaveAs();
 		}
 
+		// ### 逻辑
 		private void RandomPicture2List() {
 			m_images = Directory.EnumerateFiles(m_imageFolder).Where(file_path => {
 				String ext = file_path.Substring(file_path.LastIndexOf(".")).ToLower();
@@ -38,32 +68,46 @@ namespace PictureRandomer {
 			});
 		}
 
-		private void RandomPictureAndSave() { 
-			var image_enum = Directory.EnumerateFiles(m_imageFolder).Where(file_path => {
-				String ext = file_path.Substring(file_path.LastIndexOf(".")).ToLower();
-				return ext == ".jpg" || ext == ".bmp" || ext == ".gif";
-			});
-			int random_index = m_randomer.Next(0, image_enum.Count());
-			String real_path = image_enum.ElementAt(random_index);
-			this.pictureBox1.ImageLocation = real_path;
-			this.label1.Text = real_path;
-			Clipboard.SetText(real_path);
-			//passed_real_path.Add(real_path);
-		}
-
 		private void SetPictureBox(String fileName) {
 			this.pictureBox1.ImageLocation = fileName;
-			this.label1.Text = fileName;
+			SetText(fileName);
 			Clipboard.SetText(fileName);
 		}
 
-		private void pictureBox1_MouseClick(object sender, MouseEventArgs e) {
-			if (e.Button == System.Windows.Forms.MouseButtons.Left) {
-				imageIndex = (++imageIndex == m_images.Count ? 0 : imageIndex);
-			} else if (e.Button == System.Windows.Forms.MouseButtons.Right) {
-				imageIndex = (--imageIndex == -1 ? m_images.Count - 1 : imageIndex);
-			}
-			SetPictureBox(m_images[imageIndex]);
+		private void SetText(string text, Color color) {
+			lblMessage.ForeColor = color;
+			lblMessage.Text = (text == null ? string.Empty : text);
 		}
+
+		private void SetTextColor(Color color) {
+			lblMessage.ForeColor = color;
+		}
+
+		private void SetText(string text) {
+			SetText(text, Color.Black);
+		}
+
+		private void ClearText() {
+			SetText(string.Empty);
+		}
+
+		private void GoNextPicture() {
+			m_imageIndex = (++m_imageIndex == m_images.Count ? 0 : m_imageIndex);
+			SetPictureBox(m_images[m_imageIndex]);
+		}
+
+		private void GoPreviousPicture() {
+			m_imageIndex = (--m_imageIndex == -1 ? m_images.Count - 1 : m_imageIndex);
+			SetPictureBox(m_images[m_imageIndex]);
+		}
+
+		private void SaveAs() {
+			if (dlgPictureOutputSelector.ShowDialog() == DialogResult.OK) {
+				FileInfo file = new FileInfo(m_images[m_imageIndex]);
+				file.CopyTo(dlgPictureOutputSelector.SelectedPath + "\\" + file.Name);
+				SetTextColor(Color.Blue);
+			}
+		}
+
 	}
 }
